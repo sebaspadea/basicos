6bb68b250a05c8ac93d5203dc593776f3302a6e0
sebastian spadea <spadea.sebastian@gmail.com>
2020-08-24 17:17:50 -0300

    Terminando practica de basicos de React, creando un carrito de compras

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producto from './components/Producto';
+import Carrito from './components/Carrito';
 
 function App() {
+
+  // Obtener la fecha
+  const fecha = new Date().getFullYear();
+
+  // Crear listado de productos
+  const [ productos, guardarProductos ] = useState([
+    { id: 1, nombre: 'Camisa ReactJS', precio: 50 },
+    { id: 2, nombre: 'Camisa VueJS', precio: 40 },
+    { id: 3, nombre: 'Camisa Node', precio: 30 },
+    { id: 4, nombre: 'Camisa Angular', precio: 20 },
+  ]);
+
+  // State para un carrito de compras
+  const [ carrito, agregarProducto ] = useState([]);
+
   return (
     <Fragment>
-        <Header />
+        <Header 
+          titulo= 'Tienda Virtual'
+        />
       
-        <Footer />
+      <h1>Lista de Productos</h1>
+      {productos.map(producto => (
+        <Producto 
+            key = {producto.id}
+            producto = {producto}
+            productos={productos}
+            carrito={carrito}
+            agregarProducto={agregarProducto}
+        />
+      ))}
+        <Carrito 
+            carrito={carrito}
+            agregarProducto={agregarProducto}
+        />
+
+        <Footer 
+          fecha={fecha}
+        />
     </Fragment>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Header () {
+function Header ({titulo}) {
+
 
   return(
-    <h1 className="encabezado">Tienda Virtual</h1>
+  <h1 className="encabezado"> {titulo} </h1>
   )
 }
 
